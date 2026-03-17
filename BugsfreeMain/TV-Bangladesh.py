336289b22581fae6595d11d1de6422913b08b4b4
Bugsfree <113718082+bugsfreeweb@users.noreply.github.com>
2026-03-17 11:41:19 +0600

    Update TV-Bangladesh.py

--- BugsfreeMain/TV-Bangladesh.py
+++ BugsfreeMain/TV-Bangladesh.py
@@ -262,6 +262,7 @@ def main():
         "https://raw.githubusercontent.com/sm-monirulislam/SM-Live-TV/refs/heads/main/New_Bdix.m3u",
         "https://raw.githubusercontent.com/sm-monirulislam/SM-Live-TV/refs/heads/main/Combined_Live_TV.m3u",
         "https://raw.githubusercontent.com/sm-monirulislam/SM-Live-TV/refs/heads/main/Bangla%20Channel.m3u",
+        "https://raw.githubusercontent.com/imShakil/tvlink/refs/heads/main/all.m3u",
         "https://raw.githubusercontent.com/abusaeeidx/Mrgify-Tv/refs/heads/main/playlist.m3u",
         "https://raw.githubusercontent.com/time2shine/IPTV/refs/heads/master/combined.m3u",
         "https://raw.githubusercontent.com/mhmimxl/filoox-bdix-selected/main/playlist.m3u",
